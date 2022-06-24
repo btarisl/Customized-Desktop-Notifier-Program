8982867699ecdbd7f7ceaae1d32d238b1203203f
btarisl <108023975+btarisl@users.noreply.github.com>
2022-06-24 16:57:03 +0700

    Delete HANS_LATIHAN1_BTARI_1202213127.py

--- HANS_LATIHAN1_BTARI_1202213127.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("cls")
-#menghitung volume kubus
-sisi= float(int(input('Masukkan sisi : ')))
-volume = lambda sisi:sisi**3
-
-print(f"Volume kubus dengan sisi {sisi} adalah {volume(sisi)}")
